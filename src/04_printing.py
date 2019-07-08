d02c3fe63e815ab7aef93c503e36d7a4b2a79ff7
Pablo Ogata <pabloogata@gmail.com>
2019-07-08 16:05:23 -0400

    Update 04_printing.py

--- src/04_printing.py
+++ src/04_printing.py
@@ -13,5 +13,11 @@ z = "I like turtles!"
 # x is 10, y is 2.25, z is "I like turtles!"
 
 # Use the 'format' string method to print the same thing
+arr=[10,2.5, "I like turtles!"]
+print (arr)
 
 # Finally, print the same thing using an f-string
+arr=[10,2.5, "I like turtles!"]
+print (arr)
+
+"{0}.{1}.{2}.".format(10, 2.5, "I like turles!")
