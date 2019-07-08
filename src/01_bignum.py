6fa475a7be951609ecfecbdee2cda7e69257fc7f
Pablo Ogata <pabloogata@gmail.com>
2019-07-08 15:54:32 -0400

    Update 01_bignum.py

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,11 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+var msg = 'Hello world'
+
+
+console.log(msg)
+
+$('#msg').html(msg)
+
+//Otherwise,missing statement"
