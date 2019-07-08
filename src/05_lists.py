488c12e80cf0d3bd096cebb00067fc5e550414a9
Pablo Ogata <pabloogata@gmail.com>
2019-07-08 16:24:35 -0400

    Update 05_lists.py

--- src/05_lists.py
+++ src/05_lists.py
@@ -10,13 +10,46 @@ y = [8, 9, 10]
 # YOUR CODE HERE 
 print(x)
 
+x = [1, 2, 3]
+y = [8, 9, 10]
+
+for x in range(1, 5):
+    print (x)
+    
+    or
+    
+    
+    x = [1, 2, 3]
+y = [8, 9, 10]
+
+x.append(4)
+print (x)
+
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
 print(x)
 
+x = [1, 2, 3]
+y = [8, 9, 10]
+
+x.append(4)
+
+
+z = x + y
+
+print (z)
+
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
 print(x)
+
+x = [1, 2, 3]
+y = [8, 9, 10]
+
+x.extend([4, 9, 10])
+
+
+print (x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
@@ -24,6 +57,11 @@ print(x)
 
 # Print the length of list x
 # YOUR CODE HERE 
+x = [1, 2, 3]
+y = [8, 9, 10]
+
+x.extend([4, 9, 99, 10])
+
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
