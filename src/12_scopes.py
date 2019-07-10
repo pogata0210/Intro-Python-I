5c8c4bc3475adb30433267bc7c7251b136ef4bd5
Pablo Ogata <pabloogata@gmail.com>
2019-07-10 18:32:21 -0400

    Update 12_scopes.py

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -9,7 +9,7 @@ def changeX():
 
 changeX()
 
-# This prints 12. What do we have to modify in changeX() to get it to print 99?
+# This prints 12. What do we have to modify in changeX() to get it to print 99? -87. global  before change
 print(x)
 
 
