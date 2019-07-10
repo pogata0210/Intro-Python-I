12b5ad5eccd7720b4c72214a956c00b2890b375d
Pablo Ogata <pabloogata@gmail.com>
2019-07-10 18:30:36 -0400

    Update 11_args.py

--- src/11_args.py
+++ src/11_args.py
@@ -6,12 +6,20 @@
 
 # YOUR CODE HERE
 
+ef f1(num1, num2):
+    return num1 + num2
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*argv):
+    sum = 0
+    for arg in argv:
+        sum += arg
+    return sum
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -28,6 +36,11 @@ print(f2(a))    # Should print 22
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(num1, num2=0):
+    if num2:
+        return num1 + num2
+    else:
+        return num1 + 1
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,6 +55,10 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**param):
+    for key in param:
+        print(f'key: {key}, value: {param[key]}')
+
 
 # Should print
 # key: a, value: 12
