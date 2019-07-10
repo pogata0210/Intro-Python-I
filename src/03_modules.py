fc5ea39a63e57f61fa76e49a81bf0fcd65b3545c
Pablo Ogata <pabloogata@gmail.com>
2019-07-10 17:46:54 -0400

    Update 03_modules.py

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,11 +11,18 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
+print(sys.argv)
+
+
 # Print out the OS platform you're using:
 # YOUR CODE HERE
 
+print(sys.getwindowsversion())
+print(sys.platform)
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+print(sys.version)
+print(sys.version_info)
 
 
 import os
