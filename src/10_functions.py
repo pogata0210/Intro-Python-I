354b09349e4734bf781aec18b31a88430ab3bd40
Pablo Ogata <pabloogata@gmail.com>
2019-07-10 18:28:52 -0400

    Update 10_functions.py

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,5 +1,13 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+print(is_even(20))
+
 # YOUR CODE HERE
 
 # Read a number from the keyboard
@@ -7,6 +15,13 @@ num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
+def define_number(num):
+
+    if num %2 == 0:
+        return "Even"
+    else: return "Odd"
+
+print (define_number(12))
 
 # YOUR CODE HERE
 
